814aa935337a792e601705f98c657697f4e7129c
luxuan05 <luxuan.wee.2024@computing.smu.edu.sg>
2025-10-29 01:29:41 +0000

    ex2 done

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -6,6 +6,18 @@
                 items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
             }
             
+        },
+        methods:{
+            addItem(){
+                if (this.newItem.trim() !== ''){
+                    this.items.push(this.newItem);
+                    this.newItem = '';
+                }
+            },
+            deleteItem(index){
+                // remove the item at the specified index
+                this.items.splice(index, 1);
+            }
         }
     }
 </script>
@@ -14,5 +26,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul id="cart">
+        <li v-for="(item, index) in items">{{ item }}
+            <button @click="deleteItem(index)">Delete!</button>
+        </li>
+    </ul>
+
+    <input v-model="newItem"></input>
+    <button @click="addItem">Add!</button>
    
 </template>
